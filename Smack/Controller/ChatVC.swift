32f092284df8301caed7f72f4b50b481f5f3a69f
MISHAL ALHAJRI <mishalalhajri@MISHALs-MacBook-Air.local>
2019-07-21 20:08:26 +0300

    SWReveal Added

---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,29 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by MISHAL ALHAJRI on 18/11/1440 AH.
+//  Copyright © 1440 MISHAL ALHAJRI. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().frontViewShadowColor = UIColor.white
+        self.revealViewController().frontViewShadowColor = UIColor.black
+        self.revealViewController().frontViewShadowRadius = 10
+        self.revealViewController().frontViewShadowOpacity = 0.50
+        menuButton.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+    
+    
+
+
+
+}
---
+++ Smack/View/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by MISHAL ALHAJRI on 18/11/1440 AH.
+//  Copyright © 1440 MISHAL ALHAJRI. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
